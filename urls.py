e5afcedcad83acfc92c34219f3ff26717c17b295
shuyu <907237099@qq.com>
2018-11-13 18:01:36 +0800

    perf(urls):add urls for handlers

--- urls.py
+++ urls.py
@@ -1,8 +1,12 @@
 from handler.base import BaseHandler
-from handler.provinceHandler import ProvinceHandler
+from handler.province import ProvinceHandler
+from handler.city import LikedCityHandler
+from handler.login import LoginHandler
 from settings import settings
 
 url_patterns = [
+    (r"/login", LoginHandler),
+    (r"/username/city", LikedCityHandler),
     (r"/province/(?P<province>.*)", ProvinceHandler),
     (r"/.*", BaseHandler)
 ]
